4ea4bad21ce55d9b2be1ae211090022c52749591
diegoribe <diego@engineering.com>
2020-05-29 22:01:47 -0400

    pushing stuff

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <todos v-bind:todos='todos' v-on:del-todo="deleteTodo"/>
+  </div>
+</template>
+
+<script>
+
+import todos from './components/Todos'
+export default {
+  name: 'App',
+  components: {
+    todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'todo one',
+          completed: false
+        },
+        {
+          id:2,
+          title: 'todo two',
+          completed: false
+        },
+        {
+          id:3,
+          title: 'todo three',
+          completed: false
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteTodo: function (id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div v-bind:key="todo.id" v-for='todo in todos'> 
+            <todoitem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)" />
+        </div>
+    </div>
+</template>
+<script>
+
+import todoitem from './TodoItem.vue'
+
+export default {
+    name: 'todos',
+    components: {
+        todoitem
+    },
+    props: ['todos']
+}
+</script>
+
+<style scoped>
+
+</style>
